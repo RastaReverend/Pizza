cfc064314ad625ae7dc521da207eff4d4d3d854d
RastaReverend <80354122+RastaReverend@users.noreply.github.com>
2021-03-23 11:25:33 -0400

    Add files via upload

---
+++ pizzaColors.js
@@ -0,0 +1,7 @@
+/** A set of functions to change the background color*/
+// Change background to red
+function setRed() {pageTitle.style.background = "tomato";}
+// Change background to white
+function setWhite() {pageTitle.style.background = "beige";
+}
+
